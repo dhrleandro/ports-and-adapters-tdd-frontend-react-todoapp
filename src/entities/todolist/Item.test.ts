f5f6bd465ba3ef5ffa1c485e61dd651e1a3929d4
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2023-01-03 11:05:54 -0300

    [entities] test static create method

--- src/entities/todolist/Item.test.ts
+++ src/entities/todolist/Item.test.ts
@@ -11,3 +11,13 @@ test('create doned', () => {
   const item = new Item(undefined, 'Test Item', true);
   expect(item.done).toEqual(true);
 });
+
+test('test static create method', () => {
+  const itemA = Item.create(undefined, 'test A', true);
+  expect(itemA.description).toEqual('test A');
+  expect(itemA.done).toEqual(true);
+
+  const itemB = Item.create();
+  expect(itemB.description).toEqual('');
+  expect(itemB.done).toEqual(false);
+});
